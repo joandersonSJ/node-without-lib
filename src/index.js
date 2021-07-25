10884f60a75b2051e0f5863502dae450e875b98e
joandersonSJ <joandersonsantosdev@gmail.com>
2021-07-25 03:51:46 -0300

    :sparkles: added user's list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const http = require('http')
-
+const users = require('./mock/users')
 const server = http.createServer((request, response) => {
-  response.writeHead(200)
-  response.end('Hello World')
+  console.log(`Request method: ${request.method} | Request endpoint: ${request.url}`)
+
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(users))
 })
 
 server.listen(3000, () => console.log('Server started'))
---
+++ src/mock/users.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Scraggy'
+  },
+  {
+    id: 2,
+    name: 'Nana'
+  }
+]
